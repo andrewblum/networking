909ada120b27f60269570eba70b0b4591b21fa0a
Andrew <andrewdblum@gmail.com>
2019-06-22 15:38:37 -0700

    bytewiser exercise 4

--- bytewiser.js
+++ bytewiser.js
@@ -14,11 +14,31 @@
 // Exercise 3 
 // look at process stdin and change all . to ! and console log 
 
-process.stdin.on('data', buff => {
-  for (let i = 0; i < buff.length; i++) {
-    if (buff[i] === 46) {
-      buff[i] = 33
-    }
-  }
-  console.log(buff)
-})
+// process.stdin.on('data', buff => {
+//   for (let i = 0; i < buff.length; i++) {
+//     if (buff[i] === 46) {
+//       buff[i] = 33
+//     }
+//   }
+//   console.log(buff)
+// })
+
+
+// exercise 4 
+// const fs = require('fs');
+// filename = process.argv[2]
+// fs.readFile(filename, (err, contents) => {
+//   let start = 0;
+//   for (let i = 0; i < contents.length; i++) {
+//     if (contents[i] === 10 || i === contents.length - 1) {
+//       console.log(contents.slice(start, i))
+//       start = i + 1;
+//     }
+//   }
+// })
+
+// exercise 5 
+
+
+
+
--- rawNetworkParser.js
+++ rawNetworkParser.js
@@ -1,5 +1,30 @@
 const fs = require('fs');
  
 fs.readFile('net.cap', function(err, contents) {
-    console.log(contents);
+  // contents is a buffer of Unit8Array, 8 bit unsigned (single byte) entries.
+  //contents = contents.toString('hex')
+  console.log(contents.toString('hex', 0, 4))
+
+  for (let i = 0; i < contents.length; i++) {
+  }
+  // in hex, every 2 charecters represents 1 byte
+  // 8 hex charecters holds 4 bytes
+  // our magic number is the first 8 hex charecters 
+
+  console.log(contents[4])
+  console.log(contents[5])
+
+  console.log(contents[6])
+  console.log(contents[7])
+
+  console.log(contents[8])
+  console.log(contents[9])
+  console.log(contents[10])
+  console.log(contents[11])
+ 
 });
+
+
+function pcapHeader(contents) {
+
+}
