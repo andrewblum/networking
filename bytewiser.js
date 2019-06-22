776cc803f6e29fd7c59bcc3b3876a227665bcec8
Andrew <andrewdblum@gmail.com>
2019-06-21 19:16:26 -0700

    parse buffer as hex

--- bytewiser.js
+++ bytewiser.js
@@ -1,9 +1,16 @@
+// Exercise 1
+
 // Write a node program that prints a buffer object containing
 // the string "bytewiser" using console.log.
 
-const buf1 = Buffer.from('bytewsier');
-const buf2 = new Buffer('bytewiser');
-console.log(buf2);
+// const buf1 = Buffer.from('bytewsier');
+// const buf2 = new Buffer('bytewiser');
+// console.log(buf2);
 
+// Exercise 2
+// const input = process.argv;
+// console.log(Buffer.from(input).toString('hex').replace(/^0+/, ''))
+
+// Exercise 3 
 
 
