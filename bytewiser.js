8a73328713f7454994cc1e06806284b6ee68b6e0
Andrew <andrewdblum@gmail.com>
2019-06-21 17:40:42 -0700

    bytewiser and first buffer exercise

---
+++ bytewiser.js
@@ -0,0 +1,9 @@
+// Write a node program that prints a buffer object containing
+// the string "bytewiser" using console.log.
+
+const buf1 = Buffer.from('bytewsier');
+const buf2 = new Buffer('bytewiser');
+console.log(buf2);
+
+
+
