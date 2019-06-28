ba1602701ff35b0bd0199c236a733d5ed745ad19
Andrew <andrewdblum@gmail.com>
2019-06-28 14:33:59 -0700

    dns client start

--- rawNetworkParser.js
+++ rawNetworkParser.js
@@ -1,30 +1,34 @@
 const fs = require('fs');
  
-fs.readFile('net.cap', function(err, contents) {
+fs.readFile('net.cap', function(err, data) {
   // contents is a buffer of Unit8Array, 8 bit unsigned (single byte) entries.
-  //contents = contents.toString('hex')
-  console.log(contents.toString('hex', 0, 4))
+  // contents = contents.toString('hex')
+  pcapHeader(data)
+  data = data.slice(24, data.length)
 
-  for (let i = 0; i < contents.length; i++) {
+  console.log(data.slice(0, 1))
+
+  for (let i = 0; i < data.length; i++) {
   }
-  // in hex, every 2 charecters represents 1 byte
-  // 8 hex charecters holds 4 bytes
-  // our magic number is the first 8 hex charecters 
-
-  console.log(contents[4])
-  console.log(contents[5])
-
-  console.log(contents[6])
-  console.log(contents[7])
-
-  console.log(contents[8])
-  console.log(contents[9])
-  console.log(contents[10])
-  console.log(contents[11])
- 
 });
 
 
-function pcapHeader(contents) {
 
+
+
+function pcapHeader(contents) {
+  console.log(contents.toString('hex', 0, 4))
+  console.log(contents.slice(0, 4))
+
+  console.log(contents.slice(4, 6))
+
+  console.log(contents.slice(6, 8))
+
+  console.log(contents.slice(8, 12))
+
+  console.log(contents.slice(12, 16))
+
+  console.log(contents.slice(16, 20))
+
+  console.log(contents.slice(20, 24))
 }
