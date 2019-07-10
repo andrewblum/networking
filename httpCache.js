c5932067720385d7e921d1dbebe2dd8803a90901
Andrew <andrewdblum@gmail.com>
2019-07-09 19:35:19 -0700

    more http cache

--- httpCache.js
+++ httpCache.js
@@ -1,33 +1,49 @@
 const net = require('net');
 
-// CLIENT
+// ***** CLIENT ******
 
-const client = new net.Socket();
+// const client = new net.Socket();
 
-client.on('message', (msg, rinfo) => {
-  console.log('Got a message')
-  parseDnsResponse(msg);
-  console.log(rinfo)
-});
+// client.on('message', (msg, rinfo) => {
+//   console.log('Got a message')
+//   parseDnsResponse(msg);
+//   console.log(rinfo)
+// });
 
-client.connect(5000, '127.0.0.1', () => {
-  console.log(' client connected')
-  socket.write('hello server')
-});
+// client.connect(5000, '127.0.0.1', () => {
+//   console.log(' client connected')
+//   socket.write('hello server')
+// });
 
-client.on('data', (data) => {
-  console.log('got data:', data);
-})
+// client.on('data', (data) => {
+//   console.log('got data:', data);
+// })
 
-client.on('close', () => {
-  console.log('connection was closed')
-})
+// client.on('close', () => {
+//   console.log('connection was closed')
+// })
 
 
-// SERVER
+// ***** SERVER *****
 
-const server = netcreateServer((socekt) => {
+const server = net.createServer((socket) => {
+  console.log(socket)
+  socket.on('data', (data) => {
+    parseResponse(data)
+  })
   socket.pipe(socket);
 })
 
 server.listen(5000, '127.0.0.1')
+
+function parseResponse(data) {
+  console.log('socket data obtained', data)
+
+  // eventually.. 
+  //checkCache()
+}
+
+function checkCache(request) {
+
+}
+
