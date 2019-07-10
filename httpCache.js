f72d3e105397dc2b832bdc6c735c5f794ee8b60a
Andrew <andrewdblum@gmail.com>
2019-07-09 20:48:20 -0700

    working caching server

--- httpCache.js
+++ httpCache.js
@@ -13,6 +13,7 @@ const server = net.createServer((socket) => {
 server.listen(5000, '127.0.0.1')
 
 function parseRequest(request, socket) {
+
   if (!requestInCache(request)) {
     httpRequest(request, socket)
   } else {
@@ -20,18 +21,21 @@ function parseRequest(request, socket) {
   }
 }
 
-function httpRequest(request, socket) {
-  const client = new net.Socket();
-  
-  client.connect(5001, '127.0.0.1', () => {
-    client.write(request)
-  });
+// Connection to our web server
+const client = new net.Socket();
+client.connect(5001, '127.0.0.1', () => {});
 
+function httpRequest(request, socket) {
   client.on('data', (data) => {
     socket.write(data)
-  })  
+  }) 
+  client.write(request)
 }
 
 function requestInCache(request) {
-    return false;
+  if (CACHE[request]) {
+    return true;
+  } else {
+    CACHE[request] = request;
+  }
 }
